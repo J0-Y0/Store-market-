df3075ea3b00ec629e50988a40dfc359d0f32d98
yosef <yosfemyau@gmail.com>
2024-09-27 00:47:31 +0300

    ORM: aggregate

--- store/views.py
+++ store/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, OrderItem
 from django.db.models import Q, F
+from django.db.models.aggregates import Count
 
 
 def test(request):
-    query_set = Product.objects.values("id", "title", "price", "collection__title")
-    context = {"datas": list(query_set)}
+    query_set = Product.objects.aggregate(Count("collection"))
+
+    # values("id", "title", "price", "collection__title")
+    # query_set = Product.objects.values("collection").all()
+    context = {"datas": (query_set)}
     return render(request, "test.html", context)
