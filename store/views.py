f5fe406d7c87b57625d9d845ec219ed4437ef8d2
yosef <yosfemyau@gmail.com>
2024-09-26 23:18:56 +0300

    query set :limiting Columns

--- store/views.py
+++ store/views.py
@@ -4,6 +4,6 @@ from django.db.models import Q, F
 
 
 def test(request):
-    query_set = Product.objects.values("id", "title", "price", "collection__name")
+    query_set = Product.objects.values("id", "title", "price", "collection__title")
     context = {"datas": list(query_set)}
     return render(request, "test.html", context)
