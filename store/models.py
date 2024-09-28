757b82071c461c97f9c5128e18f2366d8e6154e7
yosef <yosfemyau@gmail.com>
2024-09-28 14:38:09 +0300

    model field validator

--- store/models.py
+++ store/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0, "price cant me negative"),
+            MaxValueValidator(100000, "unusual value"),
+        ],
+    )
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
